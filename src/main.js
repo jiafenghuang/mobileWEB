a8292bc85af927336cead61e44635b58d8c2a11a
Sheldon <39021705+jiafenghuang@users.noreply.github.com>
2020-03-13 12:30:49 +0800

    安装axios,mint-ui

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import Axios from "axios";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+import Mint from "mint-ui";
+Vue.use(Mint);
+/*
+// 按需引入部分组件
+import { Cell, Checklist } from 'mint-ui';
+Vue.component(Cell.name, Cell);
+Vue.component(Checklist.name, Checklist);
+*/
+
+Vue.prototype.$axios = Axios;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  components: { App },
-  template: '<App/>'
-})
+  components: {
+    App
+  },
+  template: "<App/>"
+});
