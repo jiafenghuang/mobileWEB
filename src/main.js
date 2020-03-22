b83efc8c20a010c0f5f38c6fff4a73c63a293db6
jiafenghuang <39021705+jiafenghuang@users.noreply.github.com>
2020-03-22 20:43:10 +0800

    添加全局过滤器和moment

--- src/main.js
+++ src/main.js
@@ -3,19 +3,24 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
+import Moment from "moment";
 import Axios from "./initAxios/initAxios";
 // import Axios from "axios";
 // Axios.defaults.baseURL = 'https://www.shangcongxuan.cn/sofa/api/fetch';
 import Qs from "qs";
 Vue.config.productionTip = false;
-import Vant from 'vant';
-import 'vant/lib/index.css';
+import Vant from "vant";
+import "vant/lib/index.css";
 Vue.use(Vant);
 /*
 // 按需引入部分组件
 */
-
-import { Toast } from 'vant';
+// 注册全局过滤器
+// Vue.filter("fullTime", function(value) {
+//   console.log("value :", value);
+//   return Moment(value).format("YYYY MM DD, HH:mm");
+// });
+import { Toast } from "vant";
 
 Vue.use(Toast);
 Vue.prototype.$axios = Axios;
